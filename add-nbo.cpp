607782af659267ea4d209ba571fdd73201c70fb0
qlsdlgh <qlsdlgh@naver.com>
2024-03-10 13:50:27 +0900

    Add add-nbo

---
+++ add-nbo.cpp
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#include "add-nbo.h"
+
+uint32_t add_nbo(uint32_t a, uint32_t b){
+    uint32_t c = a + b;
+    return c;
+}
---
+++ add-nbo.h
@@ -0,0 +1,1 @@
+uint32_t add_nbo(uint32_t a, uint32_t b);
